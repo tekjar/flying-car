649871516f27438cd945d91c29ee56ddacba37c1
raviteja <k.teza1@gmail.com>
2018-09-25 00:02:41 +0530

    4: Use the already provided 'IntegrateBodyRate' function

--- fcnd.projects/FCND-Estimation-CPP/src/QuadEstimatorEKF.cpp
+++ fcnd.projects/FCND-Estimation-CPP/src/QuadEstimatorEKF.cpp
@@ -93,10 +93,17 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
-
+  //  float predictedPitch = pitchEst + dtIMU * gyro.y;
+  //  float predictedRoll = rollEst + dtIMU * gyro.x;
+  //  ekfState(6) = ekfState(6) + dtIMU * gyro.z;    // yaw
+  
+  // Using the already provided 'IntegrateBodyRate' function
+    Quaternion<float> quadQuat = Quaternion<float>::FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+    quadQuat.IntegrateBodyRate(gyro, dtIMU);
+    float predictedPitch = quadQuat.Pitch();
+    float predictedRoll = quadQuat.Roll();
+    ekfState(6) = quadQuat.Yaw();
+    
   // normalize yaw to -pi .. pi
   if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
   if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
